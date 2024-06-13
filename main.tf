cd67da8300df1a7420621ee1a86527bf7e40946a
Yury Niño Roa <yurynino@google.com>
2024-06-13 18:37:51 -0500

    Second Commit

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+provider "google" {
+  project = "liverpool-train"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource google_compute_instance "vm_instance" {
+  name         = "vm-liverpool"
+  zone         = "us-central1"
+  machine_type = "n1-standard-1"
+  boot_disk {
+    initialize_params {
+      image    = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      # Allocate a one-to-one NAT IP to the instance
+    }
+  }
+}
+
+/*
 resource "local_file" "foo" {
   content   = "Hello World!"
   filename  = "delete_me.txt"
@@ -5,4 +29,4 @@ resource "local_file" "foo" {
 
 output "filename" {
   value   = local_file.foo.filename
-}
+}*/
