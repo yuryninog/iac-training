e133070795a608525a2bdf00a9dbb74ba4781543
Yury Niño Roa <yurynino@google.com>
2025-07-28 17:14:19 -0500

    Test python 4

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-    resource "null_resource" "run_python_script" {
-      provisioner "local-exec" {
-        command = "python3 script.py" # Replace 'your_script.py' with your script's name
-      }
-    }
+resource "null_resource" "run_python_script" {
+  provisioner "local-exec" {
+    command = "python3 script.py" # Replace 'your_script.py' with your script's name
+  }
+}
+
+resource "local_file" "foo" {
+  content  = "foo!"
+  filename = "../foo.bar"
+}
